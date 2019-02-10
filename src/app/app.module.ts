f0d8e2bf0ab4e23beec2ec910f8260ad80fd5cc0
Faisal Qureshi <faisal1899@gmail.com>
2019-02-10 23:40:23 +0530

    feat(core): add support for angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -12,7 +14,8 @@ import { APP_CONFIG, APP_DI_CONFIG } from './app-config.constants';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
   ],
   providers: [{
     provide: APP_CONFIG,
