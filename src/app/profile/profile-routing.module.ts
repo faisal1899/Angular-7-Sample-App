85a2ebc9f13910384e43eddbe691b2592a25684e
Faisal Qureshi <faisal1899@gmail.com>
2019-03-31 15:06:45 +0530

    feat(core): lazy load profile module

--- src/app/profile/profile-routing.module.ts
+++ src/app/profile/profile-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile.component';
+import { AuthGuard } from '../core/auth-guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: ProfileComponent,
+    canActivate: [AuthGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
