f82274ab5ea5d93a9091a37a8518675d357543a4
Faisal Qureshi <faisal1899@gmail.com>
2019-03-31 16:16:13 +0530

    feat(shared): add app background color directive

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { AppBackgroundColorDirective } from './directives/app-background-color.directive';
+
+@NgModule({
+  imports: [],
+  exports: [AppBackgroundColorDirective],
+  declarations: [AppBackgroundColorDirective],
+  providers: [],
+})
+export class SharedModule { }
