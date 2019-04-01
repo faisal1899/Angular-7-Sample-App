55f04ecbb8f5c9b48cddb6e22cc42866b76922ee
Faisal Qureshi <faisal1899@gmail.com>
2019-04-01 22:46:10 +0530

    feat(core): add app check user permissions directive

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 
 import { AppBackgroundColorDirective } from './directives/app-background-color.directive';
+import { CheckUserPermissionsDirective } from './directive/check-user-permissions.directive';
 
 @NgModule({
   imports: [],
-  exports: [AppBackgroundColorDirective],
-  declarations: [AppBackgroundColorDirective],
+  exports: [
+    AppBackgroundColorDirective,
+    CheckUserPermissionsDirective,
+  ],
+  declarations: [
+    AppBackgroundColorDirective,
+    CheckUserPermissionsDirective,
+  ],
   providers: [],
 })
 export class SharedModule { }
